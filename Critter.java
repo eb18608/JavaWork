4898346e5ab2d685d2b3982b239053c0460630bc
eb18608 <eb18608@bristol.ac.uk>
2019-02-07 13:37:49 +0000

    first commit

---
+++ Critter.java
@@ -0,0 +1,12 @@
+public class Critter {
+
+    static String Name = "Cow";
+
+    static void Poke()  {
+        System.out.println(Name + " was poked");
+    }
+    public static void main(String[] args) {
+        Poke();
+    }
+}
+	
